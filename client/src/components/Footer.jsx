7c2def7ca317bc6fa5c204fe82106fee286ca708
Sahil Dhoot <94707312+Sahil-2904@users.noreply.github.com>
2023-04-16 02:15:27 +0530

    Update Footer.jsx
    Added github url

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ import "../index.css";
 function Footer(){
     return (
         <footer className="bg-gradient-to-r from-cyan-700 to-blue-900 mt-10 flex justify-center flex-col">
-            <div className="flex justify-center rounded-full"><a href="/"><GitHubIcon class="social"/></a></div>
+            <div className="flex justify-center rounded-full"><a href="https://github.com/Sahil-2904/SocialBlock/tree/main"><GitHubIcon class="social"/></a></div>
             <div>
                 <p className="text-slate-300">Copyright © 2023 BlockSocial</p>
             </div>
